8dbe13157f0d258ab5774a91b2c017f1a6a1812c
Janis Lebron <janislebron@gmail.com>
2021-04-08 23:11:30 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,22 +1,21 @@
-CREATE TABLE project (
+CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
-  start_date INTEGER
-  end_date INTEGER
-  );
+  start_date DATE,
+  end_date DATE
+);
 
-
-CREATE TABLE user (
+CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-  );
+);
 
-
-CREATE TABLE pledge (
+CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   amount INTEGER,
   user_id INTEGER,
-  project_id INTEGER);
+  project_id INTEGER
+);
